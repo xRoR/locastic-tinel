f7798437684d5095bf2615afe25b051968a7b46f
xror <xror@mail.com>
2022-02-17 21:52:46 +0100

    Minor fixes

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -167,7 +167,7 @@ const Cart: React.FC<{
     isOpened ? open() : close();
   }, [isOpened, open, close]);
 
-  useScrollLock(isMobile ? true : (withScrollLock  ? isOpened : false));
+  useScrollLock(isMobile ? isOpened : (withScrollLock  ? isOpened : false));
 
   /** Renders */
   const _renderOverlay = () => {
